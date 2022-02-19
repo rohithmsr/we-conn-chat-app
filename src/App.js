9ade18fef3529ec6a1ec5517832005583942b023
rohithmsr <rohithmsr2001@gmail.com>
2022-02-19 23:52:45 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { fb } from './service/firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  useEffect(() => {
+    fb.firestore
+      .collection('chatUsers')
+      .where('userName', '==', 'test1234')
+      .get()
+      .then(res => {
+        const users = res.docs[0]?.data();
+        console.log(users);
+      });
+  }, []);
+
+  return <>Hello from Rohith M S R 👨‍💻👨‍💻</>;
+};
 
 export default App;
